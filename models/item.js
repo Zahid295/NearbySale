c78a17e864e1be4420c0eaa82d1e93cb3a1d6bd2
Zahid295 <zahid@295>
2024-06-01 16:45:37 +0100

    Add second model

---
+++ models/item.js
@@ -0,0 +1,13 @@
+// Item model
+const mongoose = require('mongoose');
+
+const ItemSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: String,
+  price: Number,
+  photos: [String],
+  location: { type: String, required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+});
+
+module.exports = mongoose.model('Item', ItemSchema);
