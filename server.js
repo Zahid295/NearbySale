5f3c959b5997d3cb1b3ae7a08d46b0afc016ab53
Zahid295 <zahid@295>
2024-06-01 15:23:37 +0100

    Set up server

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// Import Express framework
+const express = require('express');
+const app = express();
 // Connect to MongoDB Atlas
 require('dotenv').config();
 const mongoose = require('mongoose');
@@ -6,3 +9,8 @@ const uri = process.env.MONGODB_URI;
 mongoose.connect(uri)
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
+
+// Connect to server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
