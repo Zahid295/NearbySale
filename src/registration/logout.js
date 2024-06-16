11c998bcb59f93aafcdc902ff01ab19a94fcc9fc
Zahid295 <zahid@295>
2024-06-17 00:25:58 +0100

    Add logout function

--- src/registration/logout.js
+++ src/registration/logout.js
@@ -1,16 +1,9 @@
 import React from 'react';
 
-const Logout = () => {
-  const logout = () => {
-    // Delete the token from local storage
-    localStorage.removeItem('token');
-    // Direct the user to the login page
-    window.location = '/login';
-  };
-
+function Logout({ onLogout }) {
   return (
-    <button onClick={logout}>Logout</button>
+    <button onClick={onLogout}>Logout</button>
   );
-};
+}
 
 export default Logout;
