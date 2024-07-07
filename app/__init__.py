84a7cf8dbf91bde28bab67a2881451493893250c
Zahid295 <zahid@295>
2024-07-07 03:04:17 +0100

    Fix running app

--- app/__init__.py
+++ app/__init__.py
@@ -5,13 +5,12 @@ from config import SQLALCHEMY_DATABASE_URI, SECRET_KEY
 from .routes import routes_blueprint
 from .database import db
 from .admin import init_admin
-import os
 
 
 def create_app():
     app = Flask(__name__)
     # admin = Admin(app, name='nearby-backend', template_mode='bootstrap3')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = SECRET_KEY
 
