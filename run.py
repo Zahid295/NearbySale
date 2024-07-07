f6ea5ee656a08eed44c36ce2714acb63bd27a504
Zahid295 <zahid@295>
2024-07-07 01:47:48 +0100

    Update run.py file

--- run.py
+++ run.py
@@ -1,27 +1,8 @@
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_migrate import Migrate
-# from app import create_app
-# # from . import app
 from app import create_app
+
 
 app = create_app()
 
+
 if __name__ == '__main__':
     app.run(debug=False)
-
-
-# # if __name__ == '__main__':
-# #     app.run(debug=True)
-
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = " "
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# db = SQLAlchemy(app)
-# migrate = Migrate(app, db) 
-
-# app = create_app()
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
