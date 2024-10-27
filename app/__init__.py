03348bb84cbf95880c60502c79eb13b66f10f769
Zahid295 <zahid@295>
2024-10-27 01:45:17 +0000

    Fix variable name

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_migrate import Migrate
 from .extensions import login_manager
-from config import DATABASE_URI, SECRET_KEY 
+from config import DATABASE_URL, SECRET_KEY 
 from .routes import routes_blueprint
 from .database import db
 from .admin import init_admin
@@ -9,7 +9,7 @@ from .admin import init_admin
 
 def create_app():
     app = Flask(__name__)
-    app.config['DATABASE_URI'] = DATABASE_URI
+    app.config['DATABASE_URL'] = DATABASE_URL
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = SECRET_KEY
 
